ee1a535620c36777b9c9a87967dd0440290a8d9b
SysDevApolly <104022697+SysDevApollyon@users.noreply.github.com>
2022-07-17 22:21:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ choice = input("   1. ManyCensor   2. AnyWiggle\n   3. L33T5P34K    4. Img2ASCII
 if choice == "HELP":
   helper = input("What command do you need help with? (USE NUMBER) : ")
   if helper == "1":
-   print("generate text like \"||t||||h||||i||||s||\"")
+   print("generate text like \"||t||||h||||i||||s||\". For formatting issues you must type \">:<\" at the begining and end.")
   if helper == "2":
     print("Generate a wiggle copypasta using any phrase.")
   if helper == "3":
@@ -12,7 +12,7 @@ if choice == "HELP":
   if helper == "4":
     print("Convert an image (path) to ASCII.")
 if choice == "1":
- orig = input("(begin text with \">:<\" and dont add a space after.)\nTEXT: ")
+ orig = input("(begin text with \">:<\" and do NOT add a space after.)\nTEXT: ")
  spacefix_orig = orig.replace(" ", "<:>")
  spaced_spacefix = spacefix_orig.replace("", "||||")
  final = spaced_spacefix.replace("||<||||:||||>||", "||  ||")
@@ -23,21 +23,20 @@ if choice == "2":
   print(f"""
 {wiggle}\n  {wiggle}\n    {wiggle}\n        {wiggle}\n          {wiggle}\n            {wiggle}\n              {wiggle}\n                {wiggle}\n                  {wiggle}\n                    {wiggle}\n                      {wiggle}\n                        {wiggle}\n                         {wiggle}\n                           {wiggle}\n                             {wiggle}\n                           {wiggle}\n                         {wiggle}\n                        {wiggle}\n                      {wiggle}\n                    {wiggle}\n                  {wiggle}\n                {wiggle}\n              {wiggle}\n            {wiggle}\n          {wiggle}\n        {wiggle}\n      {wiggle}\n    {wiggle}\n  {wiggle}\n{wiggle}""")
 if choice == "3":
-  normal=input("insert text : ") 
+  normal=input("Translate text : ") 
   replacements=(('e','3'),('i','1'),('o','0'),('a','4'),('s','5'))
   new_string = normal
   for old, new in replacements:
     new_string = new_string.replace(old, new)
   print(new_string.upper())
 if choice == "4":
-  img = Image.open(input("IMAGE PATH: "))
+  img = Image.open(input(".img path: "))
   width, height = img.size
   aspect_ratio = height/width
   new_width = 40
   new_height = aspect_ratio * new_width * 0.55
   img = img.resize((new_width, int(new_height)))
   img = img.convert('L')
-
   pixels = img.getdata()
   chars = ["B","S","#","&","@","$","%","*","!",":","."]
   new_pixels = [chars[pixel//25] for pixel in pixels]
